c93c13720c3897db1d0e0af12710f7ce068fc35e
Ryan Weaver <ryan@thatsquality.com>
2014-01-23 13:28:39 -0600

    Install of FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
             new Acme\LiftStuffBundle\AcmeLiftStuffBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Acme/LiftStuffBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Acme\LiftStuffBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tough_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+}
