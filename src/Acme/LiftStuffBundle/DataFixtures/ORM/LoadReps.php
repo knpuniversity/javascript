5d045ca37829d9cb0e3b48279e6fbc9d560c3815
Ryan Weaver <ryan@thatsquality.com>
2014-01-24 17:12:29 -0600

    Giving users a first and last name - and giving them awesome cat names

--- src/Acme/LiftStuffBundle/DataFixtures/ORM/LoadReps.php
+++ src/Acme/LiftStuffBundle/DataFixtures/ORM/LoadReps.php
@@ -16,11 +16,26 @@ class LoadUserData implements FixtureInterface
     {
         $items = RepLog::getThingsYouCanLiftChoices();
 
+        $names = array(
+            array('Brad', 'Kitt'),
+            array('Cat', 'Middleton'),
+            array('Cindy', 'Clawford'),
+            array('Diane', 'Kitten'),
+            array('Fuzz', 'Aldrin'),
+            array('Hunter S.', 'Thomcat'),
+            array('J.R.R', 'Tollkitten'),
+            array('Madelion', 'Albright'),
+            array('Meowly,', 'Cyrus'),
+            array('Ron', 'Furgandy'),
+        );
+
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
-            $user->setUsername('user'.$i);
+            $user->setUsername(sprintf('%s_%s', $names[$i][0], $names[$i],[1]));
             $user->setEmail('user'.$i.'@gmail.com');
             $user->setPlainPassword('password');
+            $user->setFirstName($names[$i][0]);
+            $user->setLastName($names[$i][1]);
             $manager->persist($user);
 
             for ($j = 0; $j < rand(1, 5); $j++) {
--- src/Acme/LiftStuffBundle/Entity/User.php
+++ src/Acme/LiftStuffBundle/Entity/User.php
@@ -18,4 +18,44 @@ class User extends BaseUser
      */
     protected $id;
 
+    protected $firstName;
+
+    protected $lastName;
+
+    public function getAvatar()
+    {
+        return 'http://thecatapi.com/api/images/get?format=src&type=gif';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
 }
