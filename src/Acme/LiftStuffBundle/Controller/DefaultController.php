d6a4a4d0d57507c05c58d0acc6e4b4ed2ceceb9c
Ryan Weaver <ryan@thatsquality.com>
2014-01-24 17:15:49 -0600

    Adding a JSON endpoint for user-details

--- src/Acme/LiftStuffBundle/Controller/DefaultController.php
+++ src/Acme/LiftStuffBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace Acme\LiftStuffBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Acme\LiftStuffBundle\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -15,5 +17,38 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return array('name' => 'foo');
+    }
+
+    /**
+     * @Route(
+     *      "/users/{username}.{_format}",
+     *      name="user_show",
+     *      requirements= {"_format": "json"}
+     * )
+     */
+    public function userAction($username)
+    {
+        /** @var User $user */
+        $user = $this->getUserRepository()->findOneBy(array('username' => $username));
+        if (!$user) {
+            throw $this->createNotFoundException(sprintf('No user '.$username));
+        }
+
+        $details = array(
+            'username' => $user->getUsername(),
+            'firstName' => $user->getFirstName(),
+            'lastName'  => $user->getLastName(),
+            'avatar' => $user->getAvatar(),
+        );
+
+        return new JsonResponse($details);
+    }
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private function getUserRepository()
+    {
+        return $this->getDoctrine()->getRepository('AcmeLiftStuffBundle:User');
     }
 }
