451babed57c882284b278d0f6bf2aa5462de15c8
Ryan Weaver <ryan@thatsquality.com>
2016-11-11 15:05:02 -0500

    yay

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,37 +17,4 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
-
-    /**
-     * @Route(
-     *      "/users/{username}.{_format}",
-     *      name="user_show",
-     *      requirements= {"_format": "json"}
-     * )
-     */
-    public function userAction($username)
-    {
-        /** @var User $user */
-        $user = $this->getUserRepository()->findOneBy(array('username' => $username));
-        if (!$user) {
-            throw $this->createNotFoundException(sprintf('No user '.$username));
-        }
-
-        $details = array(
-            'username' => $user->getUsername(),
-            'firstName' => $user->getFirstName(),
-            'lastName'  => $user->getLastName(),
-            'avatar' => $user->getAvatar(),
-        );
-
-        return new JsonResponse($details);
-    }
-
-    /**
-     * @return \Doctrine\Common\Persistence\ObjectRepository
-     */
-    private function getUserRepository()
-    {
-        return $this->getDoctrine()->getRepository('AppBundle:User');
-    }
 }
