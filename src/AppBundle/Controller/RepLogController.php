9c04cd2e64dbf4bfb6ff4c20d6dfe049d1cd3e5f
Ryan Weaver <ryan@thatsquality.com>
2016-11-14 07:50:04 -0500

    removing edit

--- src/AppBundle/Controller/RepLogController.php
+++ src/AppBundle/Controller/RepLogController.php
@@ -25,29 +25,4 @@ class RepLogController extends BaseController
 
         return new Response(null, 204);
     }
-
-    /**
-     * @Route("/reps/{id}", name="rep_log_edit")
-     * @Method("PUT")
-     */
-    public function editRepLogAction(RepLog $repLog, Request $request)
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-        $data = json_decode($request->getContent(), true);
-        if ($data === null) {
-            throw new BadRequestHttpException('Invalid JSON');
-        }
-
-        $form = $this->createForm(RepLogType::class, $repLog);
-        $form->submit($data);
-        if (!$form->isValid()) {
-            $errors = $this->getErrorsFromForm($form);
-
-            return $this->createApiResponse([
-                'errors' => $errors
-            ], 400);
-        }
-
-        return $this->createApiResponse($repLog);
-    }
 }
