b17bcc9d15757b244799ff930c9dbb3fc9e5edaa
Ryan Weaver <ryan@thatsquality.com>
2017-03-03 14:06:32 -0500

    Step: Spread Operator: Combining arrays

--- play.js
+++ play.js
@@ -1,7 +1,5 @@
-let printThreeThings = function(thing1, thing2, thing3) {
-    console.log(thing1, thing2, thing3);
-};
-
 let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
 
-printThreeThings(...yummyThings);
+let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
+
+console.log(greatThings);
