e2cb3f456344bc1bd2f6d9d155b632d2918409d9
Ryan Weaver <ryan@thatsquality.com>
2017-03-03 14:06:32 -0500

    Step: Spread Operator: Bad copy of array

--- play.js
+++ play.js
@@ -2,4 +2,8 @@ let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
 
 let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
 
+let copyOfGreatThings = greatThings;
+copyOfGreatThings.push('summer');
+
 console.log(greatThings);
+console.log(copyOfGreatThings);
