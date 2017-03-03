5286f5a90b98fb69db9cf539a6a5a1c26f12e30f
Ryan Weaver <ryan@thatsquality.com>
2017-03-03 14:05:51 -0500

    Step: IIFE: Refactor into private helper object

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,6 +1,7 @@
 var RepLogApp = {
     initialize: function ($wrapper) {
         this.$wrapper = $wrapper;
+        Helper.initialize(this.$wrapper);
 
         this.$wrapper.find('.js-delete-rep-log').on(
             'click',
@@ -13,7 +14,7 @@ var RepLogApp = {
     },
     updateTotalWeightLifted: function () {
         this.$wrapper.find('.js-total-weight').html(
-            this._calculateTotalWeight()
+            Helper.calculateTotalWeight()
         );
     },
 
@@ -45,9 +46,18 @@ var RepLogApp = {
 
     handleRowClick: function () {
         console.log('row clicked!');
+    }
+};
+
+/**
+ * A "private" object
+ */
+var Helper = {
+    initialize: function ($wrapper) {
+        this.$wrapper = $wrapper;
     },
 
-    _calculateTotalWeight: function() {
+    calculateTotalWeight: function() {
         var totalWeight = 0;
         this.$wrapper.find('tbody tr').each(function () {
             totalWeight += $(this).data('weight');
