5aefcfc86eb853d2ce6486ab341cb66c1bf00208
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:44:15 -0400

    [tuts-apply-diff] Applying existing diff file for step require-first-webpack-config-js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where all compiled assets will be stored
+    .setOutputPath('public/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    .addEntry('rep_log', './public/assets/js/RepLogApp.js')
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
