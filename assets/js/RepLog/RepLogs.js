440b89b4a1cb6d00f5c2967de4776bb12d81d823
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:46:26 -0400

    [tuts-apply-diff] Applying existing diff file for step controlledinput-use-range-input

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -26,7 +26,7 @@ export default function RepLogs(props) {
             <h2>Lift Stuff! {heart}</h2>
 
             <input
-                type="number"
+                type="range"
                 value={numberOfHearts}
                 onChange={(e) => {
                     onHeartChange(+e.target.value);
