91bc76c34344ab6091af85f126c5309694ae5303
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:46:25 -0400

    [tuts-apply-diff] Applying existing diff file for step controlledinput-add-input-field-to-replogapp

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -25,6 +25,8 @@ export default function RepLogs(props) {
         <div className="col-md-7">
             <h2>Lift Stuff! {heart}</h2>
 
+            <input type="number" />
+
             <table className="table table-striped">
                 <thead>
                 <tr>
