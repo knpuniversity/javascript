b07c4c4a1a839ca0b82248a68dd1837c790bf769
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:46:07 -0400

    [tuts-apply-diff] Applying existing diff file for step dumbcomponent-refactor-to-functional

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class RepLogList extends Component {
-    render() {
-        const { highlightedRowId, onRowClick } = this.props;
+export default function RepLogList(props) {
+    const { highlightedRowId, onRowClick } = this.props;
 
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-        ];
+    const repLogs = [
+        { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+        { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+        { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+    ];
 
-        return (
-            <tbody>
-            {repLogs.map((repLog) => (
-                <tr
-                    key={repLog.id}
-                    className={highlightedRowId === repLog.id ? 'info' : ''}
-                    onClick={() =>  onRowClick(repLog.id)}
-                >
-                    <td>{repLog.itemLabel}</td>
-                    <td>{repLog.reps}</td>
-                    <td>{repLog.totalWeightLifted}</td>
-                    <td>...</td>
-                </tr>
-            ))}
-            </tbody>
-        )
-    }
+    return (
+        <tbody>
+        {repLogs.map((repLog) => (
+            <tr
+                key={repLog.id}
+                className={highlightedRowId === repLog.id ? 'info' : ''}
+                onClick={() =>  onRowClick(repLog.id)}
+            >
+                <td>{repLog.itemLabel}</td>
+                <td>{repLog.reps}</td>
+                <td>{repLog.totalWeightLifted}</td>
+                <td>...</td>
+            </tr>
+        ))}
+        </tbody>
+    );
 }
