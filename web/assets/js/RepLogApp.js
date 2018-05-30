7bd43f7397a3c625ae28ffd6f614f5bbdc54f542
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:43:22 -0400

    [tuts-apply-diff] Applying existing diff file for step real-object-use-extend

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -58,13 +58,15 @@
     var Helper = function ($wrapper) {
         this.$wrapper = $wrapper;
     };
-    Helper.prototype.calculateTotalWeight = function() {
-        var totalWeight = 0;
-        this.$wrapper.find('tbody tr').each(function () {
-            totalWeight += $(this).data('weight');
-        });
+    $.extend(Helper.prototype, {
+        calculateTotalWeight: function() {
+            var totalWeight = 0;
+            this.$wrapper.find('tbody tr').each(function () {
+                totalWeight += $(this).data('weight');
+            });
 
-        return totalWeight;
-    };
+            return totalWeight;
+        }
+    });
 
 })(window, jQuery);
