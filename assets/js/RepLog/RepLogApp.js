591f95eb67c42f8bbaa5008ff72fce0fc27587f4
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:46:01 -0400

    [tuts-apply-diff] Applying existing diff file for step collection-render-inline

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -13,17 +13,6 @@ export default class RepLogApp extends Component {
             { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
         ];
 
-        const repLogElements = repLogs.map((repLog) => {
-            return (
-                <tr key={repLog.id}>
-                    <td>{repLog.itemLabel}</td>
-                    <td>{repLog.reps}</td>
-                    <td>{repLog.totalWeightLifted}</td>
-                    <td>...</td>
-                </tr>
-            )
-        });
-
         return (
             <div className="col-md-7">
                 <h2>Lift Stuff! {heart}</h2>
@@ -38,7 +27,16 @@ export default class RepLogApp extends Component {
                     </tr>
                     </thead>
                     <tbody>
-                    {repLogElements}
+                    {repLogs.map((repLog) => {
+                        return (
+                            <tr key={repLog.id}>
+                                <td>{repLog.itemLabel}</td>
+                                <td>{repLog.reps}</td>
+                                <td>{repLog.totalWeightLifted}</td>
+                                <td>...</td>
+                            </tr>
+                        )
+                    })}
                     </tbody>
                     <tfoot>
                     <tr>
