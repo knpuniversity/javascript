82fffe521716c663c37dd9670ecc4f259fe0bfa5
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:44:05 -0400

    [tuts-apply-diff] Applying existing diff file for step weakmap-use-only-object-keys

--- play.js
+++ play.js
@@ -1,13 +1,13 @@
 let foods = new WeakMap();
-foods.set('italian', 'gelato');
-foods.set('mexican', 'tortas');
-foods.set('canadian', 'poutine');
+foods.set(['italian'], 'gelato');
+foods.set(['mexican'], 'tortas');
+foods.set(['canadian'], 'poutine');
 
 let southernUsStates = ['Tennessee', 'Kentucky', 'Texas'];
 foods.set(southernUsStates, 'hot chicken');
 
 console.log(
-    foods.get('italian'),
+    foods.get(['italian']),
     foods.get(southernUsStates),
     foods.size
 );
