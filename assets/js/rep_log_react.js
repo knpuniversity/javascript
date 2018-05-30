2ddf26c7553ad720fa9990eb5cc9c0fe81ac4754
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:45:54 -0400

    [tuts-apply-diff] Applying existing diff file for step jsx-create-first-class-component

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = <h2>Lift Stuff! <span>❤️</span></h2>;
+class RepLogApp extends React.Component {
+    render() {
+        return <h2>Lift Stuff! <span>❤️</span></h2>;
+    }
+}
 
-console.log(el);
-ReactDom.render(el, document.getElementById('lift-stuff-app'));
+console.log(<RepLogApp />);
+ReactDom.render(<RepLogApp />, document.getElementById('lift-stuff-app'));
