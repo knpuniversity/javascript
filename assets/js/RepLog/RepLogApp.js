5fd7f4a5bcdf6eba93f8b6607d77b6d6fde2dfea
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:46:10 -0400

    [tuts-apply-diff] Applying existing diff file for step container-create-replogs-prez

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RepLogList from './RepLogList';
+import RepLogs from './RepLogs';
 import PropTypes from 'prop-types';
 
 export default class RepLogApp extends Component {
@@ -21,76 +22,7 @@ export default class RepLogApp extends Component {
         const { highlightedRowId } = this.state;
         const { withHeart } = this.props;
 
-        let heart = '';
-        if (withHeart) {
-            heart = <span>❤️</span>;
-        }
-
-        return (
-            <div className="col-md-7">
-                <h2>Lift Stuff! {heart}</h2>
-
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>What</th>
-                        <th>How many times?</th>
-                        <th>Weight</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <RepLogList
-                        highlightedRowId={highlightedRowId}
-                        onRowClick={this.handleRowClick}
-                    />
-                    <tfoot>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <th>Total</th>
-                        <th>TODO</th>
-                        <td>&nbsp;</td>
-                    </tr>
-                    </tfoot>
-                </table>
-
-                <form className="form-inline">
-                    <div className="form-group">
-                        <label className="sr-only control-label required"
-                               htmlFor="rep_log_item">
-                            What did you lift?
-                        </label>
-                        <select id="rep_log_item"
-                                name="item"
-                                required="required"
-                                className="form-control">
-                            <option value="">What did you
-                                lift?
-                            </option>
-                            <option value="cat">Cat</option>
-                            <option value="fat_cat">Big Fat Cat</option>
-                            <option value="laptop">My Laptop</option>
-                            <option value="coffee_cup">Coffee Cup</option>
-                        </select>
-                    </div>
-                    {' '}
-                    <div className="form-group">
-                        <label className="sr-only control-label required"
-                               htmlFor="rep_log_reps">
-                            How many times?
-                        </label>
-                        <input type="number" id="rep_log_reps"
-                               name="reps" required="required"
-                               placeholder="How many times?"
-                               className="form-control"/>
-                    </div>
-                    {' '}
-                    <button type="submit" className="btn btn-primary">I Lifted
-                        it!
-                    </button>
-                </form>
-            </div>
-
-        );
+        return <RepLogs/>
     }
 }
 
