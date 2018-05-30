4b656fe9283156b581a670146075909761a83ff7
Ryan Weaver <ryan@thatsquality.com>
2018-05-30 15:44:00 -0400

    [tuts-apply-diff] Applying existing diff file for step spread-operator-copy-via-spread-op

--- play.js
+++ play.js
@@ -2,7 +2,7 @@ let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
 
 let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
 
-let copyOfGreatThings = greatThings;
+let copyOfGreatThings = [...greatThings];
 copyOfGreatThings.push('summer');
 
 console.log(greatThings);
