1a1efbda6b43e47409c5dc58fa8e05ce7548f4c1
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:56 -0400

    Step 329: Fetch: Polyfill fetch

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,6 +5,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import '../css/main.scss';
+import 'whatwg-fetch';
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
