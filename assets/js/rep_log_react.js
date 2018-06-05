ea46738e9ca0903af9f44ba23da0daebe920b3ad
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:05 -0400

    Step 237: First React: createElement with nested

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = React.createElement('h2', null, 'Lift History!');
+const el = React.createElement(
+    'h2',
+    null,
+    'Lift History! ',
+    React.createElement('span', null, '❤️')
+);
 
+console.log(el);
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
