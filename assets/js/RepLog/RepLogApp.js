ef9c3a538ee9343728303a236318f418dc61694b
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:12 -0400

    Step 250: Collection: Copy in table

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -8,7 +8,31 @@ export default class RepLogApp extends Component {
         }
 
         return (
-            <h2>Lift Stuff! {heart}</h2>
+            <div className="col-md-7 js-rep-log-table">
+                <h2>Lift Stuff! {heart}</h2>
+
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>What</th>
+                        <th>How many times?</th>
+                        <th>Weight</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <th>Total</th>
+                        <th className="js-total-weight"></th>
+                        <td>&nbsp;</td>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+
         );
     }
 }
