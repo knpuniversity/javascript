7c042f4e9423d3bf86cf9bcb11a171241afb1c87
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:52:35 -0400

    Step 183: Changes: Feature changes before webpack

---
+++ web/assets/js/layout.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function(window, $) {
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+})(window, jQuery);
---
+++ web/assets/js/login.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function(window, $) {
+    $(document).ready(function() {
+        $('.js-recommended-login').on('click', '.js-show-login', function(e) {
+            e.preventDefault();
+
+            $('.js-recommended-login-details').toggle();
+        });
+
+        $('.js-login-field-username').on('keydown', function(e) {
+            const $usernameInput = $(e.currentTarget);
+            // remove any existing warnings
+            $('.login-long-username-warning').remove();
+
+            if ($usernameInput.val().length >= 20) {
+                const $warning = $('<div class="login-long-username-warning">This is a really long username - are you sure that is right?</div>');
+                $usernameInput.before($warning);
+            }
+        });
+    });
+})(window, jQuery);
