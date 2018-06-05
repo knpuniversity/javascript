d82091b279e9790593cfc79311c16ab625b7eb83
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:54:05 -0400

    Step 344: Initial Data: Also pass withHeart

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import RepLogApp from './RepLog/RepLogApp';
 
-const shouldShowHeart = true;
+const shouldShowHeart = false;
 
 render(
     <RepLogApp
