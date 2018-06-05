179ac9a80130b5c55ddade26f919436aa41e42a6
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:52:48 -0400

    Step 209: CSS: Require fontawesome

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -3,6 +3,7 @@
 const $ = require('jquery');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('../css/main.css');
 
 $(document).ready(function() {
