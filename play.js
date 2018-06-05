8a2a2a93afde42fc784c7fa43092b66896667085
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:52:16 -0400

    Step 147: Spread Operator: Passing 4th item

--- play.js
+++ play.js
@@ -2,6 +2,6 @@ let printThreeThings = function(thing1, thing2, thing3) {
     console.log(thing1, thing2, thing3);
 };
 
-let yummyThings = ['pizza', 'gelato', 'sushi'];
+let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
 
 printThreeThings(...yummyThings);
