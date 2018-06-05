3c6fe945f7c730fe17c3be5c6193d79ceae88b90
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:05 -0400

    Step 238: JSX: Show first JSX!

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = React.createElement(
-    'h2',
-    null,
-    'Lift History! ',
-    React.createElement('span', null, '❤️')
-);
+const el = <h2>Lift Stuff! <span>❤️</span></h2>;
 
 console.log(el);
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
