a355419b63c950201d2599a27f51455bb7167793
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:51:34 -0400

    Step 67: Use API: Send JSON Form Submit

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -63,19 +63,21 @@
             e.preventDefault();
 
             var $form = $(e.currentTarget);
-            var $tbody = this.$wrapper.find('tbody');
-            var self = this;
+            var formData = {};
+            $.each($form.serializeArray(), function(key, fieldData) {
+                formData[fieldData.name] = fieldData.value
+            });
             $.ajax({
-                url: $form.attr('action'),
+                url: $form.data('url'),
                 method: 'POST',
-                data: $form.serialize(),
+                data: JSON.stringify(formData),
                 success: function(data) {
-                    $tbody.append(data);
-                    self.updateTotalWeightLifted();
+                    // todo
+                    console.log('success!');
                 },
                 error: function(jqXHR) {
-                    $form.closest('.js-new-rep-log-form-wrapper')
-                        .html(jqXHR.responseText);
+                    // todo
+                    console.log('error :(');
                 }
             });
         }
