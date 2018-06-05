3beb8650a2ee6da6b34c981c81ea1cba044bee40
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:54 -0400

    Step 325: Fetch: Wrapper doFetch function

--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -1,21 +1,24 @@
+function doFetch(url, options) {
+    return fetch(url, Object.assign({
+        credentials: 'same-origin',
+    }, options))
+        .then(response => {
+            return response.json();
+        });
+}
+
 /**
  * Returns a promise where the data is the rep log collection
  *
  * @return {Promise<Response>}
  */
 export function getRepLogs() {
-    return fetch('/reps', {
-        credentials: 'same-origin'
-    })
-        .then(response => {
-            return response.json();
-        })
+    return doFetch('/reps')
         .then(data => data.items);
 }
 
 export function deleteRepLog(id) {
-    return fetch(`/reps/${id}`, {
-        credentials: 'same-origin',
+    return doFetch(`/reps/${id}`, {
         method: 'DELETE'
     });
 }
