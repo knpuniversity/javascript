2189bae97e1219eafcbd9adf9d0d2aff0cfaa822
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:52:51 -0400

    Step 213: Sass: Try to use Sass

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -4,7 +4,7 @@ const $ = require('jquery');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 require('font-awesome/css/font-awesome.css');
-require('../css/main.css');
+require('../css/main.scss');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
