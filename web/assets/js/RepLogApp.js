156d8dcca591aa643554a94231d6bd90d16675d1
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:51:40 -0400

    Step 79: Promises: Using .catch instead

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -103,7 +103,7 @@
                 console.log(data);
 
                 return data;
-            }, function(jqXHR) {
+            }).catch(function(jqXHR) {
                 console.log('failed!');
                 console.log(jqXHR.responseText);
             }).then(function(data) {
