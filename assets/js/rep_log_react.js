361ac534c667f4b8d8fe6287602dc05ff334c9d6
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:09 -0400

    Step 244: JSX: Export & import module

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,11 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-
-class RepLogApp extends Component {
-    render() {
-        return <h2>Lift Stuff! <span>❤️</span></h2>;
-    }
-}
+import RepLogApp from './RepLog/RepLogApp';
 
 console.log(<RepLogApp />);
 render(<RepLogApp />, document.getElementById('lift-stuff-app'));
