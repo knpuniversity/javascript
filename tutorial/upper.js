f5b8990d23e5ba083ca8d68420740c0e5cabed3a
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:52:37 -0400

    Step 187: Changes: Cleanup

--- tutorial/upper.js
+++
@@ -1,5 +0,0 @@
-function upper(template, ...expressions) {
-    return template.reduce((accumulator, part, i) => {
-        return accumulator + (expressions[i - 1].toUpperCase ? expressions[i - 1].toUpperCase() : expressions[i - 1]) + part
-    })
-}
