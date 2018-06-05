b0347c7047636448fba5c3d2637b23260f65f646
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:52:41 -0400

    Step 195: Outside Deps: Install SweetAlert

--- public/assets/js/RepLogApp.js
+++ public/assets/js/RepLogApp.js
@@ -2,8 +2,9 @@
 
 const Helper = require('./RepLogHelper');
 const $ = require('jquery');
+const swal = require('sweetalert2');
 
-(function(window, Routing, swal) {
+(function(window, Routing) {
 
     let HelperInstances = new WeakMap();
 
@@ -212,4 +213,4 @@ const $ = require('jquery');
 `;
 
     window.RepLogApp = RepLogApp;
-})(window, Routing, swal);
+})(window, Routing);
