c6fd1ce3680c9d10aa05ec5114b6fb9769161d51
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:21 -0400

    Step 264: Child Element: Create RepLogList

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RepLogList from './RepLogList';
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -22,12 +23,6 @@ export default class RepLogApp extends Component {
             heart = <span>❤️</span>;
         }
 
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-        ];
-
         return (
             <div className="col-md-7">
                 <h2>Lift Stuff! {heart}</h2>
@@ -41,20 +36,7 @@ export default class RepLogApp extends Component {
                         <th>&nbsp;</th>
                     </tr>
                     </thead>
-                    <tbody>
-                    {repLogs.map((repLog) => (
-                        <tr
-                            key={repLog.id}
-                            className={highlightedRowId === repLog.id ? 'info' : ''}
-                            onClick={(event) => this.handleRowClick(repLog.id, event)}
-                        >
-                            <td>{repLog.itemLabel}</td>
-                            <td>{repLog.reps}</td>
-                            <td>{repLog.totalWeightLifted}</td>
-                            <td>...</td>
-                        </tr>
-                    ))}
-                    </tbody>
+                    <RepLogList/>
                     <tfoot>
                     <tr>
                         <td>&nbsp;</td>
---
+++ assets/js/RepLog/RepLogList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class RepLogList extends Component {
+    render() {
+        const repLogs = [
+            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+        ];
+
+        return (
+            <tbody>
+            {repLogs.map((repLog) => (
+                <tr
+                    key={repLog.id}
+                    className={highlightedRowId === repLog.id ? 'info' : ''}
+                    onClick={(event) => this.handleRowClick(repLog.id, event)}
+                >
+                    <td>{repLog.itemLabel}</td>
+                    <td>{repLog.reps}</td>
+                    <td>{repLog.totalWeightLifted}</td>
+                    <td>...</td>
+                </tr>
+            ))}
+            </tbody>
+        )
+    }
+}
