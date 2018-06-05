17c84b2c966db6cf32513e3bb23a3feb74f18c71
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:25 -0400

    Step 271: PropTypes: Specify RepLogList types

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function RepLogList(props) {
     const { highlightedRowId, onRowClick } = props;
@@ -26,3 +27,8 @@ export default function RepLogList(props) {
         </tbody>
     );
 }
+
+RepLogList.propTypes = {
+    highlightedRowId: PropTypes.any,
+    onRowClick: PropTypes.func
+};
