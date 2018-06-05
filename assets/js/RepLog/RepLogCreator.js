a98b261653278092c2407acd2ac9a1af3ad6bc8b
Ryan Weaver <ryan@thatsquality.com>
2018-06-05 14:53:34 -0400

    Step 289: Form: Change RepLogCreator to class

--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 function handleFormSubmit(event) {
@@ -8,44 +8,46 @@ function handleFormSubmit(event) {
     console.log(event.target.elements.namedItem('reps').value);
 }
 
-export default function RepLogCreator(props) {
-    return (
-        <form className="form-inline" onSubmit={handleFormSubmit}>
-            <div className="form-group">
-                <label className="sr-only control-label required"
-                       htmlFor="rep_log_item">
-                    What did you lift?
-                </label>
-                <select id="rep_log_item"
-                        name="item"
-                        required="required"
-                        className="form-control">
-                    <option value="">What did you
-                        lift?
-                    </option>
-                    <option value="cat">Cat</option>
-                    <option value="fat_cat">Big Fat Cat</option>
-                    <option value="laptop">My Laptop</option>
-                    <option value="coffee_cup">Coffee Cup</option>
-                </select>
-            </div>
-            {' '}
-            <div className="form-group">
-                <label className="sr-only control-label required"
-                       htmlFor="rep_log_reps">
-                    How many times?
-                </label>
-                <input type="number" id="rep_log_reps"
-                       name="reps" required="required"
-                       placeholder="How many times?"
-                       className="form-control"/>
-            </div>
-            {' '}
-            <button type="submit" className="btn btn-primary">I Lifted
-                it!
-            </button>
-        </form>
-    );
+export default class RepLogCreator extends Component {
+    render() {
+        return (
+            <form className="form-inline" onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                    <label className="sr-only control-label required"
+                           htmlFor="rep_log_item">
+                        What did you lift?
+                    </label>
+                    <select id="rep_log_item"
+                            name="item"
+                            required="required"
+                            className="form-control">
+                        <option value="">What did you
+                            lift?
+                        </option>
+                        <option value="cat">Cat</option>
+                        <option value="fat_cat">Big Fat Cat</option>
+                        <option value="laptop">My Laptop</option>
+                        <option value="coffee_cup">Coffee Cup</option>
+                    </select>
+                </div>
+                {' '}
+                <div className="form-group">
+                    <label className="sr-only control-label required"
+                           htmlFor="rep_log_reps">
+                        How many times?
+                    </label>
+                    <input type="number" id="rep_log_reps"
+                           name="reps" required="required"
+                           placeholder="How many times?"
+                           className="form-control"/>
+                </div>
+                {' '}
+                <button type="submit" className="btn btn-primary">I Lifted
+                    it!
+                </button>
+            </form>
+        );
+    }
 }
 
 RepLogCreator.propTypes = {
